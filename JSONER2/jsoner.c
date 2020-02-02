2fac7694d942843a01dac4513c71d941d4f498a3
Seyyed Parsa Neshaei <spneshaei@gmail.com>
2020-02-02 18:45:57 +0330

    First commit

--- JSONER2/jsoner.c
+++ JSONER2/jsoner.c
@@ -1,16 +1,9 @@
 //
-//  main.c
+//  jsoner.c
 //  JSONER2
 //
 //  Created by Seyyed Parsa Neshaei on 2/2/20.
 //  Copyright © 2020 Seyyed Parsa Neshaei. All rights reserved.
 //
 
-#include <stdio.h>
 #include "jsoner.h"
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
---
+++ JSONER2/jsoner.h
@@ -0,0 +1,18 @@
+//
+//  jsoner.h
+//  JSONER2
+//
+//  Created by Seyyed Parsa Neshaei on 2/2/20.
+//  Copyright © 2020 Seyyed Parsa Neshaei. All rights reserved.
+//
+
+#ifndef jsoner_h
+#define jsoner_h
+
+#include <stdio.h>
+
+#define NESTING_LEVELS 32
+
+enum NodeType { NODETYPE_OBJECT = 1, NODETYPE_ARRAY = 2 };
+
+#endif /* jsoner_h */
